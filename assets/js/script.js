935bd8a39da350d58cec794ea2452b1f94b79f11
rashu-pro <rashu.pro@gmail.com>
2023-04-13 19:30:56 +0600

    layout is ready

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,21 @@
 $('.select-2').select2({
   minimumResultsForSearch: -1
 });
+
+$(document).on('click', '.btn-navigation-js', function (e){
+  e.preventDefault();
+  let self = $(this);
+  self.closest('.step-box').removeClass('active');
+  self.attr('data-action') === 'increase' ? self.closest('.step-box').next().addClass('active') : '';
+  self.attr('data-action') === 'decrease' ? self.closest('.step-box').prev().addClass('active') : '';
+})
+
+$(document).on('click', '.box-amount-js', function (e){
+  e.preventDefault();
+  let self = $(this);
+  if(self.hasClass('active')) return;
+
+  $('.box-amount-js').removeClass('active');
+  self.addClass('active');
+  $('.donation-amount-js').val(self.attr('data-amount'));
+})
